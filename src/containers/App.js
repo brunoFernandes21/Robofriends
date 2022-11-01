1576a44b725f6d872587785c58d1ffdde6c7026e
brunoFernandes21 <47353649+brunoFernandes21@users.noreply.github.com>
2022-11-01 20:18:32 +0000

    added errorBoundary component to manage error

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,55 +1,58 @@
-import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import './App.css'
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import "./App.css";
+import ErrorBoundary from "../components/ErrorBoundary";
 // import Scroll from '../components/Scroll'
 // import { render } from '@testing-library/react';
 
-
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: '',
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
 
-    async componentDidMount() {
-        // IMPORTANT: USING .THEN
-        // fetch('https://jsonplaceholder.typicode.com/users')
-        //     .then(response => response.json())
-        //     .then(users => this.setState({robots: users}))
+  async componentDidMount() {
+    // IMPORTANT: USING .THEN
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({robots: users}))
 
-        // USING ASYNC AWAIT 
-        const fetchUsers = await fetch('https://jsonplaceholder.typicode.com/users');
-        const users = await fetchUsers.json();
-        this.setState({robots: users})
-    }
+    // USING ASYNC AWAIT
+    const fetchUsers = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    const users = await fetchUsers.json();
+    this.setState({ robots: users });
+  }
 
-    onInputChange = (event) => {
-        this.setState({searchField: event.target.value});
-    }
+  onInputChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
 
-    render() {
-        const { robots, searchField } = this.state;
-        const filteredRobots = robots.filter((robot) => {
-            return robot.name.toLowerCase()
-            .includes(searchField.toLowerCase())
-        });
-        return !robots.length ? <h1>Loading</h1> :
-         (
-            <div className='tc'>
-                <h1 className='f2'>RoboFriends</h1>
-                <SearchBox inputChange={this.onInputChange} />
-                {/* <Scroll> */}
-                    <CardList robots={filteredRobots}/> 
-                {/* </Scroll> */}
-            </div>
-               
-        )
-    }
-    
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f2">RoboFriends</h1>
+        <SearchBox inputChange={this.onInputChange} />
+        {/* <Scroll> */}
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+
+        {/* </Scroll> */}
+      </div>
+    );
+  }
 }
 
 export default App;
